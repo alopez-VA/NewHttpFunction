38c7409dc0b9270814d83c814a98a131baf79754
OITLEALOPEZA <OITLEALOPEZA@LEA-LT216020>
2024-01-26 06:06:16 -0600

    First Push

--- Functions.cs
+++ Functions.cs
@@ -5,13 +5,13 @@ using Microsoft.Extensions.Logging;
 
 namespace NewHttpFunction
 {
-    public class Function1
+    public class Functions
     {
         private readonly ILogger _logger;
 
-        public Function1(ILoggerFactory loggerFactory)
+        public Functions(ILoggerFactory loggerFactory)
         {
-            _logger = loggerFactory.CreateLogger<Function1>();
+            _logger = loggerFactory.CreateLogger<Functions>();
         }
 
         [Function("Function1")]
